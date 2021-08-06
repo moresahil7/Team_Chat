443ca3803c02f62934098cc4f132acf9f6fe44a8
Sahil <moresahil4502@gmail.com>
2021-08-06 15:01:16 +0530

    Started Creating Team_Chat  application

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4V7-Qjro5vKENZP288_hEOTTHjiPzI2c",
+  authDomain: "slack-clone-b0fa1.firebaseapp.com",
+  projectId: "slack-clone-b0fa1",
+  storageBucket: "slack-clone-b0fa1.appspot.com",
+  messagingSenderId: "375547647359",
+  appId: "1:375547647359:web:11af8c3701d3c2966c364a"
+};
