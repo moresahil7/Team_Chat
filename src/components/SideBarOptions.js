398844a7724f7bf80f02e720fa4c97194b75f66c
Sahil <moresahil4502@gmail.com>
2021-08-14 16:52:25 +0530

    created sidebar options

--- src/components/SideBarOptions.js
+++ src/components/SideBarOptions.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const SideBarOptions = (props) => {
+const SideBarOptions = ({Icon , title}) => {
     return (
         <SideBarOptionContainer>
-            
+            {Icon && <Icon fontSize = "small" style ={{padding: 10}}/>}
+
+            {Icon ?(
+                <h4>{title}</h4>
+            ):(
+                <SideBarOptionChannel>
+                    <span>#</span>{title}
+                </SideBarOptionChannel>
+            )}
         </SideBarOptionContainer>
     )
 }
@@ -16,4 +24,23 @@ export default SideBarOptions;
 
 
 const SideBarOptionContainer = styled.div`
+    display: flex;
+    font-size: 12px;
+    align-items: center;
+    padding-left: 2px;
+    cursor: pointer;
+
+    :hover{
+        opacity:0.6 ;
+        background-color: gray;
+    }
+
+    >h4 {
+        font-weight: 500;
+    }
+    >h4>span {
+        padding: 15px;
+       }
+`;
+const SideBarOptionChannel = styled.div`
 `;
